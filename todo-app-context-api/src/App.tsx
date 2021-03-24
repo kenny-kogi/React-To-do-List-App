092e660a913803fec12a571a393a9da07695871d
kenny-kogi <mwangiken497@gmail.com>
2021-03-24 16:16:34 +0300

    todo app react

---
+++ todo-app-context-api/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import TodoContextApi from './components/contextApiComponents/TodoContextApi';
+import TodoProvider from './components/contextApiComponents/TodoProvider';
+import Header from './components/Header/Header';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <TodoProvider>
+              <TodoContextApi />
+      </TodoProvider>
+    </div>
+  );
+}
+export {}
+
+export default App;
